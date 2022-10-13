3db76ca4d608ca12162f437194a89823a26b5959
Bagus Dermawan <72683434+goodgenerous@users.noreply.github.com>
2022-10-13 22:24:33 +0700

    Add files via upload

---
+++ Demo.h
@@ -0,0 +1,46 @@
+#pragma once
+#include "RenderEngine.h"
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/gtx/vector_angle.hpp>
+#include <SOIL/SOIL.h>
+
+class Demo :
+	public RenderEngine
+{
+public:
+	Demo();
+	~Demo();
+private:
+	GLuint shaderProgram, VBO, VAO, EBO, texture, VBO2, VAO2, EBO2, texture2;
+	float viewCamX, viewCamY, viewCamZ, upCamX, upCamY, upCamZ, posCamX, posCamY, posCamZ, CAMERA_SPEED, fovy;
+	float angle = 0;
+	float position = -1;
+	float position2 = -2;
+	float initpos = 0;
+	float initpos2 = 5;
+	virtual void Init();
+	virtual void DeInit();
+	virtual void Update(double deltaTime);
+	virtual void Render();
+	virtual void ProcessInput(GLFWwindow *window);
+	void BuildColoredCube();
+	void BuildColoredCube2();
+	void BuildColoredCube3();
+	void BuildColoredCube4();
+	void BuildColoredCube5();
+	void BuildColoredPlane();
+	void DrawColoredCube();
+	void DrawColoredCube2();
+	void DrawColoredCube3();
+	void DrawColoredCube4();
+	void DrawColoredCube5();
+	void DrawColoredPlane();
+	void MoveCamera(float speed);
+	void StrafeCamera(float speed);
+	void RotateCamera(float speed);
+	void InitCamera();
+
+};
+
